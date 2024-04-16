89853a0cb02ae56f054cd7723afbf25fe5aa1fd1
Antonio58RU <antonbo2004@gmail.com>
2024-04-17 00:00:06 +0300

    Seven commit

--- app/handlers.py
+++ app/handlers.py
@@ -6,8 +6,11 @@ import app.keybords as kb
 
 import requests 
 
-router = Router()
+cryptocurrencies = ["BTC", "ETH", "USDT", "BNB", "SOL"]
+prices = {}
+changes24h = {}
 
+router = Router()
 
 @router.message(CommandStart())
 async def cmd_start(message: Message):
@@ -16,27 +19,24 @@ async def cmd_start(message: Message):
     
 @router.message(F.text == 'Статистика Binance')
 async def get_stats(message: Message):
+    await message.answer(get_messageStats(), reply_markup=kb.mainIn, parse_mode="html")
     
+@router.callback_query(F.data == 'updateStats')
+async def update_stats(callback: CallbackQuery):
+    await callback.answer('')
+    await callback.message.edit_text(get_messageStats(), reply_markup=kb.mainIn, parse_mode="html")
+    
+def get_messageStats():
     r = requests.get('https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,USDT,BNB,SOL&tsyms=USD')
     json_data = r.json()
 
-    priceBTC = round(json_data["RAW"]["BTC"]["USD"]["PRICE"], 2)
-    priceETH = round(json_data["RAW"]["ETH"]["USD"]["PRICE"], 2)
-    priceUSDT = round(json_data["RAW"]["USDT"]["USD"]["PRICE"], 2)
-    priceBNB = round(json_data["RAW"]["BNB"]["USD"]["PRICE"], 2)
-    priceSOL = round(json_data["RAW"]["SOL"]["USD"]["PRICE"], 2)
-    
-    changePct24HourBTC = round(json_data["RAW"]["BTC"]["USD"]["CHANGEPCT24HOUR"], 2)
-    changePct24HourETH = round(json_data["RAW"]["ETH"]["USD"]["CHANGEPCT24HOUR"], 2)
-    changePct24HourUSDT = round(json_data["RAW"]["USDT"]["USD"]["CHANGEPCT24HOUR"], 2)
-    changePct24HourBNB = round(json_data["RAW"]["BNB"]["USD"]["CHANGEPCT24HOUR"], 2)
-    changePct24HourSOL = round(json_data["RAW"]["SOL"]["USD"]["CHANGEPCT24HOUR"], 2)
-
-    
-    await message.answer(f"🏦<b>CryptoStats</b>\n\n<b>BTC:=</b><i>{priceBTC}$</i>, <b>24h:</b> <i>{changePct24HourBTC}%</i>\n<b>ETH:=</b> <i>{priceETH}$</i>, <b>24h:</b> <i>{changePct24HourETH}%</i>\n<b>USDT:=</b> <i>{priceUSDT}$</i>, <b>24h:</b> <i>{changePct24HourUSDT}%</i>\n<b>BNB:=</b> <i>{priceBNB}$</i>, <b>24h:</b> <i>{changePct24HourBNB}%</i>\n<b>SOL:=</b> <i>{priceSOL}$</i>, <b>24h:</b> <i>{changePct24HourSOL}%</i>",reply_markup=kb.mainIn, parse_mode="html")
-    
-@router.callback_query(F.data == 'updateStats')
-async def updateStats(callback: CallbackQuery):
-    pass
-    
-    
+    for currency in cryptocurrencies:
+        price = json_data["RAW"][currency]["USD"]["PRICE"]
+        prices[currency] = round(price, 2)
+        
+    for currency in cryptocurrencies:
+        change24h = json_data["RAW"][currency]["USD"]["CHANGEPCT24HOUR"]
+        changes24h[currency] = round(change24h, 2)
+        
+    messageStats_text = f"🏦<b>CryptoStats</b>\n\n<b>BTC:=</b><i>{prices['BTC']}$</i>, <b>24h:</b> <i>{changes24h['BTC']}%</i>\n<b>ETH:=</b> <i>{prices['ETH']}$</i>, <b>24h:</b> <i>{changes24h['ETH']}%</i>\n<b>USDT:=</b> <i>{prices['USDT']}$</i>, <b>24h:</b> <i>{changes24h['USDT']}%</i>\n<b>BNB:=</b> <i>{prices['BNB']}$</i>, <b>24h:</b> <i>{changes24h['BNB']}%</i>\n<b>SOL:=</b> <i>{prices['SOL']}$</i>, <b>24h:</b> <i>{changes24h['SOL']}%</i>"
+    return messageStats_text
